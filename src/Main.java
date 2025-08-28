a92facf75832c5c694e7e4ad60c4bedc6328b723
HAROLD LOPEZ <harold_lopezfo@fet.edu.co>
2025-08-28 12:34:31 -0500

    Agregar un lista

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        ArrayList nums = new ArrayList();
     }
 }
