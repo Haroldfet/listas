ba0994338081e04e1e09bcb789ebed24bb6e240c
HAROLD LOPEZ <harold_lopezfo@fet.edu.co>
2025-08-28 23:55:02 -0500

    Agregar un lista

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList nums = new ArrayList();
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(12);
+        nums.add(15);
+        nums.add(35);
+        nums.add(48);
+
+        System.out.println("elementos de la posición:" + nums.get(3));
+        System.out.println("el tamaño de la lista es:" + nums.size());
     }
-}
+}   
