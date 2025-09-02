7ad5941b33ae8d3b8379b10796c629a75884d4c3
HAROLD LOPEZ <harold_lopezfo@fet.edu.co>
2025-09-02 00:25:25 -0500

    Agregar un lista

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,49 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<>();
+
+        List<Integer> nums = new ArrayList<>();
         nums.add(12);
         nums.add(15);
         nums.add(35);
         nums.add(48);
+        nums.add(60);
+        nums.add(75);
 
-        System.out.println("elementos de la posición:" + nums.get(3));
-        System.out.println("el tamaño de la lista es:" + nums.size());
+
+        int suma = 0;
+        for (int n : nums) {
+            suma += n;
+        }
+        System.out.println("Suma de nums: " + suma);
+
+
+        List<String> nombres = new ArrayList<>();
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("Carla");
+        nombres.add("Diego");
+
+
+        System.out.println("Impresión con for:");
+        for (int i = 0; i < nombres.size(); i++) {
+            System.out.println(nombres.get(i));
+        }
+
+
+        System.out.println("Impresión con foreach:");
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+
+
+        System.out.println("Impresión con while:");
+        int i = 0;
+        while (i < nombres.size()) {
+            System.out.println(nombres.get(i));
+            i++;
+        }
     }
-}   
+}
